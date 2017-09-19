b9ece1e509809285345f5d6ce50f5b67f40441d0
SetYourUserName <stefanmiljakovic@yahoo.com>
2017-09-19 09:17:05 +0200

    Initial commit

---
+++ app/src/main/java/com/week1/practice1/git/MainActivity.java
@@ -0,0 +1,13 @@
+package com.week1.practice1.git;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
